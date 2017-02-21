687e19add65e59711bc43c089b9766c6910be9e2
vdt-mik <vdt-mik@alterpost.org>
2017-02-21 11:18:20 +0200

    fix Mysql users from apache_1

--- DevOps Course/lab1/db/mysql_joomla_setting.sh
+++ DevOps Course/lab1/db/mysql_joomla_setting.sh
@@ -6,7 +6,7 @@ GRANT ALL PRIVILEGES ON joomla.* TO 'joomla'@'localhost';
 CREATE USER 'joomla'@'10.0.0.12' IDENTIFIED BY 'joomla';
 CREATE USER 'joomla'@'10.0.0.13' IDENTIFIED BY 'joomla';
 GRANT SELECT,DELETE,INSERT,UPDATE ON joomla.* TO 'joomla'@'10.0.0.12';
-GRANT SELECT,DELETE,INSERT,UPDATE ON joomla.* TO 'joomla'@'10.0.0.12';
+GRANT SELECT,DELETE,INSERT,UPDATE ON joomla.* TO 'joomla'@'10.0.0.13';
 FLUSH PRIVILEGES;
 EOF
 mysql -u root -pvagrant joomla < /vagrant/db/backup_joomla.sql
